8359f9c2b0972886de48e5d44f15df34901d3947
Piensom <56769500+piensom@users.noreply.github.com>
2022-08-22 22:29:23 -0600

    Delete vendor directory

--- vendor/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import vendors
-from . import sale_order_extends
-from . import res_partner_extends
--- vendor/models/res_partner_extends.py
+++
@@ -1,8 +0,0 @@
-from odoo import api, fields, models, _
-
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    vendors = fields.Many2one(string='Vendedor', comodel_name='vendor.model')
-
